bd40c1d2265122de5bd8c6dfecb78a9d4dd122f5
SayounalaQAQ <lzaisinile1221@outlook.com>
2026-03-09 21:51:25 +0800

    Add files via upload
    初识1

---
+++ main.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("haahia");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 20;
+//	float b = 2.2;
+//	printf("%f",a+b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d %d", &num1, &num2);
+//	int sum = num1 + num2;
+//	printf("%d", sum);
+//	return 0;
+//}
+#include<stdio.h>
+int a = 20;
+void test()
+{
+	printf("test-->%d\n", a);
+
+}
+int main()
+{
+	test();
+
+	{
+	
+
+		printf("a=%d\n", a);
+
+	}
+
+
+	printf("a=%d\n", a);
+
+	return 0;
+}
